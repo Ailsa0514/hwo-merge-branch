f583288b59bda79fe8ada3f34fd507836e0318d2
lqq <ailsa_000@163.com>
2019-09-04 18:09:44 +0800

    修改页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link>&nbsp;&
       <router-link to="/about">About</router-link>&nbsp;&
       <router-link to="/detail">Detail</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
